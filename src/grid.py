1b8b7aa0ef8fea6ebf6540a8ecc6ab61457ecbb3
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2024-10-30 11:18:02 -0700

    define some arithmetic operations for grid cells

--- src/grid.py
+++ src/grid.py
@@ -13,9 +13,10 @@ from action import (
     Orientation,
 )
 
+STRICT_CELL_ARITHMETIC = True
 
-# use a list instead of a dict because we can index into this list with the GridCell since it's an integere backed Enum
-CellChars = ["+", "x", ".", "#"]
+# use a list instead of a dict because we can index into this list with the GridCell since it's an integer backed Enum
+CellChars = [".", "+", "x", "", "#"]
 
 
 class GridCell(Enum):
@@ -23,14 +24,47 @@ class GridCell(Enum):
     The possible states of a cell in the grid
     """
 
-    # red and blue are 0 and 1 so that they line up with the player IDs
-    RED = 0
-    BLUE = 1
-    EMPTY = 2
-    NEUTRAL = 3
+    # the values are chosen such that the empty cell is 0 and the other cells are powers of 2,
+    # this is useful for arithmetic operations on the cells as we can use bitwise operations
+    EMPTY = 0b0000
+    RED = 0b0001
+    BLUE = 0b0010
+    NEUTRAL = 0b0100
 
     def __str__(self) -> str:
         return CellChars[self.value]
+
+    def __sub__(self, other: object) -> "GridCell":
+        if not isinstance(other, GridCell):
+            return NotImplemented
+        match self, other:
+            case a, b if a == b:
+                return GridCell.EMPTY
+            case a, GridCell.EMPTY:
+                return a
+            case GridCell.EMPTY, b:
+                if STRICT_CELL_ARITHMETIC:
+                    raise ValueError(
+                        f"Subtracting {b} from an empty cell is not allowed"
+                    )
+                return GridCell.EMPTY
+            case a, b:
+                if STRICT_CELL_ARITHMETIC:
+                    raise ValueError(f"Subtracting {b} from {a} is not allowed")
+                return GridCell.EMPTY
+
+    def __add__(self, other: object) -> "GridCell":
+        if not isinstance(other, GridCell):
+            return NotImplemented
+        match self, other:
+            case GridCell.EMPTY, b:
+                return b
+            case a, GridCell.EMPTY:
+                return a
+            case a, b:
+                if STRICT_CELL_ARITHMETIC:
+                    raise ValueError(f"Adding {a} and {b} is not allowed")
+                return GridCell.EMPTY
 
 
 @dataclass
@@ -253,6 +287,15 @@ class Grid:
                     self.neutral_positions[0] == other.neutral_positions[1]
                     and self.neutral_positions[1] == other.neutral_positions[0]
                 )
+            )
+        )
+
+    def __hash__(self) -> int:
+        return hash(
+            (
+                self.red_position,
+                self.blue_position,
+                self.neutral_positions,
             )
         )
 
